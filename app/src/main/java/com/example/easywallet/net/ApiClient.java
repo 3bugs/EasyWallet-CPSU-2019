f5b1fa07ef1d86474ac548b213f52af33044792a
Promlert Lovichit <promlert@gmail.com>
2019-10-27 14:26:34 +0700

    เพิ่มไฟล์ ApiClient.java

---
+++ app/src/main/java/com/example/easywallet/net/ApiClient.java
@@ -0,0 +1,40 @@
+package com.example.easywallet.net;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static final String BASE_URL = "http://localhost/easy_wallet/";
+    private static final String API_BASE_URL = BASE_URL + "api.php/";
+    //public static final String IMAGE_BASE_URL = BASE_URL + "images/";
+
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        final OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .readTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .build();
+
+        if (retrofit == null) {
+            Gson gson = new GsonBuilder()
+                    .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                    .create();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    //.addConverterFactory(ScalarsConverterFactory.create())
+                    .client(okHttpClient)
+                    .build();
+        }
+        return retrofit;
+    }
+}
